c42bb432a541ffe50de9268beb8c85f474577e9e
Samantha Lincroft <petraarkian.heg.gov@gmail.com>
2018-01-03 14:58:31 -0500

    added random word fields

--- index.js
+++ index.js
@@ -12,12 +12,21 @@ const form = document.getElementById("form")
 const song = document.getElementById("song")
 const artist = document.getElementById("artist")
 const lyricsEl = document.getElementById("lyrics")
+const w1 = document.getElementById("w1")
+const w2 = document.getElementById("w2")
+const w3 = document.getElementById("w3")
+const w4 = document.getElementById("w4")
+
 
 form.onsubmit = (event) => {
   console.log("HELLO")
   event.preventDefault()
   console.log(song.value)
   console.log(artist.value)
+  console.log(w1.value)
+  console.log(w2.value)
+  console.log(w3.value)
+  console.log(w4.value)
   getLyrics()
 }
 
@@ -25,14 +34,20 @@ async function getLyrics() {
     try {
       let response = await fetch("https://api.lyrics.ovh/v1/" + artist.value + "/" + song.value.replace(" ", "+"))
       let lyricObj = await response.json()
-      lyricsEl.innerText = lyricObj.lyrics
+      lyricsEl.innerText = newLyrics(lyricObj.lyrics)
       console.log("LYRICSSSS?")
-
       console.log(lyrics)
     } catch (error) {
       console.log(error)
     }
 }
+
+function newLyrics(lyrics){
+  console.log(getFrequency(lyrics))
+  return lyrics
+}
+
+
 
 
 //
