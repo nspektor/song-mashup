925a341df3f20aaa3e027355e3ea1eff69beb2c5
Nellie Spektor <nellie.spektor@gmail.com>
2018-01-03 14:56:00 -0500

    word counting

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function getLyrics() {
       let response = await fetch("https://api.lyrics.ovh/v1/" + artist.value + "/" + song.value.replace(" ", "+"))
       let lyricObj = await response.json()
       lyricsEl.innerText = lyricObj.lyrics
+      getFreqs(lyricObj.lyrics)
       console.log("LYRICSSSS?")
 
       console.log(lyrics)
@@ -33,40 +34,40 @@ async function getLyrics() {
       console.log(error)
     }
 }
+// Taken from: https://stackoverflow.com/questions/30906807/word-frequency-in-javascript
+var getFreqs = (lyricStr) => {
+    // lyricStr.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    // lyricStr.toLowerCase()
+    /* Below is a regular expression that finds alphanumeric characters
+       Next is a string that could easily be replaced with a reference to a form control
+       Lastly, we have an array that will hold any words matching our pattern */
+    var pattern = /\w+/g,
+        string = lyricStr.replace(/[.,\/#!$%\^&\*;:{}=\-_`'~()]/g,"").toLowerCase(),
+        matchedWords = string.match( pattern );
 
+    /* The Array.prototype.reduce method assists us in producing a single value from an
+       array. In this case, we're going to use it to output an object with results. */
+    var counts = matchedWords.reduce(function ( stats, word ) {
 
-//
-// var printSongs = () => {
-//   const songNames = (data) => {
-//   names = .people.map((person) => {
-//     return person.name
-//   })
-//   return names
-//   // return an array containing the name strings of the astronauts in space
-// }
-//
-// getData = () =>{
-//   fetch(URL OF API)
-//   .then(r => r.json()) //json.parse r which is response of fetch, to make usable js object
-//   .then(astros => renderAstros(astros)) //response of r.json (so its a js object) thing is fed into renderAstros
-// }
-//
-// // new fancy fetch
-// // async:
-//
-// asynch function getAstros(){
-//   try{
-//     let response = await fetch(link)
-//     let astros = await response.json()
-//     renderAstros(astros)
-//   } catch (error){
-//     console.log(error)
-//   }
-//
-//   }
-// }
-//
-//
-//
-// //JSON.stringify(object) js object -> JSON
-// //JSON.parse(jsonObject) JSON -> js object
+        /* `stats` is the object that we'll be building up over time.
+           `word` is each individual entry in the `matchedWords` array */
+        if ( stats.hasOwnProperty( word ) ) {
+            /* `stats` already has an entry for the current `word`.
+               As a result, let's increment the count for that `word`. */
+            stats[ word ] = stats[ word ] + 1;
+        } else {
+            /* `stats` does not yet have an entry for the current `word`.
+               As a result, let's add a new entry, and set count to 1. */
+            stats[ word ] = 1;
+        }
+
+        /* Because we are building up `stats` over numerous iterations,
+           we need to return it for the next pass to modify it. */
+        return stats;
+
+    }, {} );
+
+    /* Now that `counts` has our object, we can log it. */
+    console.log( counts );
+
+}
